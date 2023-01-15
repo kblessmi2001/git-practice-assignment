1bc0858ea0e3be30c8ee4bbc772148395c1848f4
Blessmi K <kblessmi2001@gmail.com>
2023-01-15 21:21:02 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,16 @@
+  let count=1;
+    for(let i=1;i<num;i++)
+    {
+        if(num%i==1)
+        {
+            count++
+        }
+    }
+    if(count=2)
+    {
+        console.log("Yes")
+    }
+    else
+    {
+        console.log("No")
+    }
